f9c1893f4ecbe36eb8dbfa4694ce65961faa9a9f
Nolan Warner <renrawnalon@gmail.com>
2015-12-22 20:21:26 +0900

    Updated Playground to be more playgroundesque and show meaningful info

--- ArgoPlayground.playground/Contents.swift
+++ ArgoPlayground.playground/Contents.swift
@@ -3,20 +3,12 @@
 import Argo
 import Curry
 
-struct UserList {
-    let users: [User]
-}
-
-extension UserList: Decodable {
-    static func decode(json: JSON) -> Decoded<UserList> {
-        return curry(UserList.init)
-            <^> json <|| "users"
-    }
-}
+//: User to decode with Argo.
 
 struct User {
     let userId: Int
-    let name: String
+    let firstName: String
+    let lastName: String
     let email: String?
     let companyName: String
     let friends: [User]?
@@ -26,188 +18,166 @@ extension User: Decodable {
     static func decode(json: JSON) -> Decoded<User> {
         return curry(User.init)
             <^> json <| "id"
-            <*> json <| "name"
-            <*> json <|? "email" // Use ? for parsing optional values
-            <*> json <| "company" // Parse nested objects
-            <*> json <||? "friends" // parse arrays of objects
+            <*> json <| "firstName"
+            <*> json <| "lastName"
+            <*> json <|? "email"
+            <*> json <| "company"
+            <*> json <||? "friends"
     }
 }
 
-var georgeJson: AnyObject {
-    return [
+//: Group to decode containing a list of Users.
+
+struct Group {
+    let users: [User]
+}
+
+extension Group: Decodable {
+    static func decode(json: JSON) -> Decoded<Group> {
+        return curry(Group.init)
+            <^> json <|| "users"
+    }
+}
+
+//: Setup json objects to play around with.
+
+var georgeJson: AnyObject = [
         "id":1,
-        "name":"George",
+        "firstName":"George",
+        "lastName":"Washington",
         "email":"email1@gmail.com",
         "company":"Company",
         "friends": [
             [
                 "id":2,
-                "name":"Larry",
+                "firstName":"Larry",
+                "lastName":"Walker",
                 "email":"email2@gmail.com",
                 "company":"Company",
                 "friends": []
             ],
             [
                 "id":3,
-                "name":"Tom",
+                "firstName":"Tom",
+                "lastName":"Smith",
                 "company":"Company",
                 "friends": []
             ]
         ]
     ]
-}
 
-var larryJson: AnyObject {
-    return [
-        "id":2,
-        "name":"Larry",
-        "email":"email2@gmail.com",
-        "company":"Company",
-        "friends": [
-            [
-                "id":1,
-                "name":"George",
-                "email":"email1@gmail.com",
-                "company":"Company",
-                "friends": []
-            ]/*,
-            [
-            "id":3,
-            "name":"Tom",
+var larryJson: AnyObject = [
+    "id":2,
+    "firstName":"Larry",
+    "lastName":"Walker",
+    "email":"email2@gmail.com",
+    "company":"Company",
+    "friends": [
+        [
+            "id":1,
+            "firstName":"George",
+            "lastName":"Washington",
+            "email":"email1@gmail.com",
             "company":"Company",
             "friends": []
-            ]*/
         ]
     ]
-}
+]
 
-var tomJson: AnyObject {
-    return [
-        "id":3,
-        "name":"Tom",
-        "company":"Company",
-        "friends": [
-            [
-                "id":1,
-                "name":"George",
-                "email":"email1@gmail.com",
-                "company":"Company",
-                "friends": []
-            ]/*,
-            [
-            "id":2,
-            "name":"Larry",
-            "email":"email2@gmail.com",
+var tomJson: AnyObject = [
+    "id":3,
+    "firstName":"Tom",
+    "lastName":"Smith",
+    "company":"Company",
+    "friends": [
+        [
+            "id":1,
+            "firstName":"George",
+            "lastName":"Washington",
+            "email":"email1@gmail.com",
             "company":"Company",
             "friends": []
-            ]*/
         ]
     ]
-}
+]
 
-var invalidJson: AnyObject {
-    return [
-        "id":3,
-        "company":"Company",
-        "friends": [
-            [
-                "id":1,
-                "name":"George",
-                "email":"email1@gmail.com",
-                "company":"Company",
-                "friends": []
-            ]
+var invalidJson: AnyObject = [
+    "id":3,
+    "company":"Company",
+    "lastName":"NoFirstName",
+    "friends": [
+        [
+            "id":1,
+            "firstName":"George",
+            "lastName":"Washington",
+            "email":"email1@gmail.com",
+            "company":"Company",
+            "friends": []
         ]
     ]
+]
+
+//: Decode json object nesting in array.
+
+var jsonString: String = "{\"id\":3, \"firstName\":\"Steve\", \"lastName\":\"Macintoch\", \"company\":\"Company\", \"friends\": null}"
+
+let array = Array<AnyObject>(arrayLiteral: georgeJson, larryJson, tomJson)
+let json = ["users": array] as AnyObject
+if let group: Group = decode(json) {
+    group.users[0].userId
+    group.users[0].firstName
+    group.users[0].lastName
+    group.users[0].email
+    group.users[0].companyName
+    group.users[0].friends
+
+    group.users[1].userId
+    group.users[1].firstName
+    group.users[1].lastName
+    group.users[1].email
+    group.users[1].companyName
+    group.users[1].friends
+
+    group.users[2].userId
+    group.users[2].firstName
+    group.users[2].lastName
+    group.users[2].email
+    group.users[2].companyName
+    group.users[2].friends
 }
 
+//: Decode single json object.
 
-
-var jsonString: String {
-    return " {\"id\":3, \"name\":\"Steve\", \"company\":\"Company\", \"friends\": null } "
+if let user: User = decode(tomJson) {
+    user.userId
+    user.firstName
+    user.lastName
+    user.email
+    user.companyName
+    user.friends
 }
 
-func jsonManipulationWithStrings() {
-    guard
-        let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding),
-        let json: AnyObject = try? NSJSONSerialization.JSONObjectWithData(data, options: []) else {
-            print("Invalid json string.")
-            return
-    }
-    
-    print("The Json:")
-    print(json)
-    
-    if let user: Decoded<User> = decode(json) {
-        print("****")
-        print(user)
-    }
+//: Decode child object of jsonDictionary.
+
+let tomsFriends = tomJson["friends"] as! [AnyObject]
+
+if let user: User = decode(tomsFriends[0]) {
+    user.userId
+    user.firstName
+    user.lastName
+    user.email
+    user.companyName
+    user.friends
 }
 
-func jsonManipulationWithDicts() {
-    let array = Array<AnyObject>(arrayLiteral: georgeJson, larryJson, tomJson)
-    let json = ["users": array] as AnyObject
-    if let userList: UserList = decode(json) {
-        print("****")
-        for user in userList.users {
-            printUser(user)
-        }
-    }
-    
-    if let user: User = decode(tomJson) {
-        print("****")
-        printUser(user)
-    }
-    
-    let tomsFriends = tomJson["friends"] as! [AnyObject]
-    print("****")
-    for tomsFriend in tomsFriends {
-        if let friend: User = decode(tomsFriend) {
-            printUser(friend)
-        }
-    }
-    
-    let invalidUser: Decoded<User> = decode(invalidJson)
-    let invalidName = invalidUser.map({ (user) -> String in
-        return user.name
-    })
-    print("****")
-    print(invalidName)
-    
-    let validUser: Decoded<User> = decode(tomJson)
-    let validName = validUser.map({ (user) -> String in
-        return user.name
-    })
-    print("****")
-    print(validName)
-}
+//: Try out mapping result of Decoding.
 
-func printUser(user: User) {
-    print("====")
-    print(user.userId)
-    print(user.name)
-    if let email = user.email {
-        print(email)
-    }
-    print(user.companyName)
-    
-    guard let friends = user.friends else {
-        print("Friend Count: 0")
-        return
-    }
-    
-    print("Friend Count: \(friends.count)")
-    
-    for friend in friends {
-        print("    ----")
-        print("    \(friend.userId)")
-        print("    " + friend.name)
-        if let email = friend.email {
-            print("    " + email)
-        }
-        print("    " + friend.companyName)
-    }
-}
+let validUser: Decoded<User> = decode(tomJson)
+validUser.map({ (user) -> String in
+    return user.firstName + " " + user.lastName
+})
 
-jsonManipulationWithStrings()
-jsonManipulationWithDicts()
+let invalidUser: Decoded<User> = decode(invalidJson)
+invalidUser.map({ (user) -> String in
+    return user.firstName + " " + user.lastName
+})
