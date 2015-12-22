d33f6d97fdfe581d143f714f2b331340395bbe46
Nolan Warner <renrawnalon@gmail.com>
2015-12-22 11:51:31 +0900

    Added UserModels

--- ArgoTest/ViewController.swift
+++ ArgoTest/ViewController.swift
@@ -7,19 +7,132 @@
 //
 
 import UIKit
+import Argo
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let array = Array<AnyObject>(arrayLiteral: georgeJson, larryJson, tomJson)
+        let json = ["users": array] as AnyObject
+        if let userList: UserList = decode(json) {
+            print("success")
+            print("****")
+            for user in userList.users {
+                printUser(user)
+            }
+        }
+        
+        if let user: User = decode(tomJson) {
+            print("****")
+            printUser(user)
+        }
+        
+        let tomsFriends = tomJson["friends"] as! [AnyObject]
+        print("****")
+        for tomsFriend in tomsFriends {
+            if let friend: User = decode(tomsFriend) {
+                printUser(friend)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func printUser(user: User) {
+        print("====")
+        print(user.userId)
+        print(user.name)
+        if let email = user.email {
+            print(email)
+        }
+        print(user.companyName)
+        print("Friend Count: \(user.friends.count)")
+        
+        for friend in user.friends {
+            print("    ----")
+            print("    \(friend.userId)")
+            print("    " + friend.name)
+            if let email = friend.email {
+                print("    " + email)
+            }
+            print("    " + friend.companyName)
+        }
     }
-
-
+    
+    var georgeJson: AnyObject {
+        let json = [
+            "id":1,
+            "name":"George",
+            "email":"email1@gmail.com",
+            "company":"Company",
+            "friends": [
+                [
+                    "id":2,
+                    "name":"Larry",
+                    "email":"email2@gmail.com",
+                    "company":"Company",
+                    "friends": []
+                ],
+                [
+                    "id":3,
+                    "name":"Tom",
+                    "company":"Company",
+                    "friends": []
+                ]
+            ]
+        ]
+        
+        return json
+    }
+    
+    var larryJson: AnyObject {
+        let json = [
+            "id":2,
+            "name":"Larry",
+            "email":"email2@gmail.com",
+            "company":"Company",
+            "friends": [
+                [
+                    "id":1,
+                    "name":"George",
+                    "email":"email1@gmail.com",
+                    "company":"Company",
+                    "friends": []
+                ]/*,
+                [
+                "id":3,
+                "name":"Tom",
+                "company":"Company",
+                "friends": []
+                ]*/
+            ]
+        ]
+        
+        return json
+    }
+    
+    var tomJson: AnyObject {
+        let json = [
+            "id":3,
+            "name":"Tom",
+            "company":"Company",
+            "friends": [
+                [
+                    "id":1,
+                    "name":"George",
+                    "email":"email1@gmail.com",
+                    "company":"Company",
+                    "friends": []
+                ]/*,
+                [
+                "id":2,
+                "name":"Larry",
+                "email":"email2@gmail.com",
+                "company":"Company",
+                "friends": []
+                ]*/
+            ]
+        ]
+        
+        return json
+    }
 }
-
